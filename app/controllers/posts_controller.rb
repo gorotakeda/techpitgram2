33ed845a36b02e071b23c8376044f88d5b6496d2
gorotakeda <rugbytkg3@gmail.com>
2021-09-25 14:22:06 +0900

    postコントローラー作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+    @post.photos.build
+    # buildもnewと同じくインスタンスを作成するメソッド
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.photos.present?
+      @post.save
+      redirect_to root_path
+      flash[:notice] = "投稿が完了しました"
+    else
+      redirect_to root_path
+      flash[:alert] = "投稿に失敗しました"
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:caption, photos_attributes: [:image]).merge(user_id: current_user.id)
+    end
 end
