7e616ee3665271506759281d112462654551e73b
gorotakeda <rugbytkg3@gmail.com>
2021-09-25 14:12:08 +0900

    posts routes 設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get '/users/:id', to: 'users#show', as: 'user'
+
+
+resources :posts, only: [:new, :create] do
+  resources :photos, only: [:create]
+end
 end
